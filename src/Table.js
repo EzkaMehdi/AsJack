9bcbfc4b92e79075ed53fffde7896924a89d9088
Mehdi <ezka.mehdi@gmail.com>
2021-04-27 11:57:39 +0200

    renderGame , allegement du else du bas de page

--- src/Table.js
+++ src/Table.js
@@ -159,51 +159,60 @@ class Table extends React.Component {
     })
   }
 
-  // render de base de la page 
+  // retour du else   
+
+renderGame() {
+  return (
+     <div>
+
+    <div className="playGame">
+      <div style={{ height: '100vh', position: 'relative' }}>
+        <h1 style={{ color: '#feb236', textAlign: 'center' }}>Black Jack</h1>
+        <Cartes key={"dealer"} cardList={this.state.dealerCardList} />
+        {this.state.endGame && (<div className='winlost'>
+          <h1>Winner is {this.state.nameOfWinner}</h1>
+        </div>)}
+        <Cartes key={"player"} cardList={this.state.playerCardList} />
+
+        <div style={{ bottom: '20px', position: 'absolute' }} className="row col-6 offset-3 flex d-flex justify-content-between">
+          <div className="d-grid gap-2">
+            <Button
+              onClick={this.onClickGive}
+              classe="btn btn-outline-warning btn-lg rounded-pill"
+              color="white"
+              bcolor="#0d6efd"
+              name="Give"
+            />
+          </div>
+          <div>
+          </div>
+          <div className="d-grid gap-2">
+            <Button
+              onClick={this.onClickStop}
+              classe="btn btn-outline-warning btn-lg rounded-pill"
+              color="white"
+              bcolor="#dc3545"
+              name="Stop"
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+// rendu de la page
 
   render() {
     if (this.state.startGame === false) {
       return (
         <Game startGame={this.startGame} />
       )
-    } else {
-      return (<div>
-
-        <div className="playGame">
-          <div style={{ height: '100vh', position: 'relative' }}>
-            <h1 style={{ color: '#feb236', textAlign: 'center' }}>Black Jack</h1>
-            <Cartes key={"dealer"} cardList={this.state.dealerCardList} />
-            {this.state.endGame && (<div className='winlost'>
-              <h1>Winner is {this.state.nameOfWinner}</h1>
-            </div>)}
-            <Cartes key={"player"} cardList={this.state.playerCardList} />
-
-            <div style={{ bottom: '20px', position: 'absolute' }} className="row col-6 offset-3 flex d-flex justify-content-between">
-              <div className="d-grid gap-2">
-                <Button
-                  onClick={this.onClickGive}
-                  classe="btn btn-outline-warning btn-lg rounded-pill"
-                  color="white"
-                  bcolor="#0d6efd"
-                  name="Give"
-                />
-              </div>
-              <div>
-              </div>
-              <div className="d-grid gap-2">
-                <Button
-                  onClick={this.onClickStop}
-                  classe="btn btn-outline-warning btn-lg rounded-pill"
-                  color="white"
-                  bcolor="#dc3545"
-                  name="Stop"
-                />
-              </div>
-
-            </div>
-          </div>
-        </div>
-      </div>
+    } else{
+      return(
+        <div>{this.renderGame()}</div>
       )
     }
   }
